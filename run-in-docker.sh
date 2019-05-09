9410c1d2a35411dcc85dde8db9cc15597adbc63f
Paul Pollack <ppollack@yahoo-inc.com>
2019-05-08 22:25:27 -0400

    remove volume till it's needed

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p8000:8000 -p9000:9000 -v `pwd`:/proxy -v `pwd`/../CryptoCoinCurrencies/CryptoTrends/my-app:/kyle node bash
+docker run -it --rm -p8000:8000 -p9000:9000 -v `pwd`:/proxy node bash
