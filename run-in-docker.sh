4c3bb605f4b7e6416a00509e104be13c38730592
Paul Pollack <ppollack@yahoo-inc.com>
2019-05-08 22:24:32 -0400

    replace hardcoded paths with dynamic ones

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p8000:8000 -p9000:9000 -v /Users/ppollack/dev/proxy-fun:/proxy -v /Users/ppollack/dev/CryptoCoinCurrencies/CryptoTrends/my-app:/kyle node bash
+docker run -it --rm -p8000:8000 -p9000:9000 -v `pwd`:/proxy -v `pwd`/../CryptoCoinCurrencies/CryptoTrends/my-app:/kyle node bash
